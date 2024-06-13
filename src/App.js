7709ec84370f82009717241f1e8f6f99efa59208
mudit raj sade <muditrajsade89@gmail.com>
2024-06-13 09:37:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 import { Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
+import Create_account from './components/create_account';
+import Video_upload from './components/video_upload';
+
+import Videos from './components/video_search';
 function App() {
   return (
     <div>
       <Routes>
-        <Route index element={<Home/>} />
+        <Route index element = {<Home/>} />
+        <Route path='/crt_account' element={<Create_account/>} />
+        <Route path='/vid_upload' element={<Video_upload/>} />
+        <Route path='/vid_search' element={<Videos/>} />
       </Routes>
     </div>
   );
